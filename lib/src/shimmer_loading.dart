73ee4b17b346accc1058860120d04a7bbf01a04d
Bellér Csaba <beller.csaba911@gmail.com>
2024-06-16 16:58:42 +0200

    Created a simple shimmer loading effect

---
+++ lib/src/shimmer_loading.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+const _shimmerGradient = LinearGradient(
+  colors: [
+    Color(0xFFEBEBF4),
+    Color(0xFFF4F4F4),
+    Color(0xFFEBEBF4),
+  ],
+  stops: [
+    0.1,
+    0.3,
+    0.4,
+  ],
+  begin: Alignment(-1.0, -0.3),
+  end: Alignment(1.0, 0.3),
+  tileMode: TileMode.clamp,
+);
+
+class _SlidingGradientTransform extends GradientTransform {
+  const _SlidingGradientTransform({
+    required this.slidePercent,
+  });
+
+  final double slidePercent;
+
+  @override
+  Matrix4? transform(Rect bounds, {TextDirection? textDirection}) {
+    return Matrix4.translationValues(bounds.width * slidePercent, 0.0, 0.0);
+  }
+}
+
+class Shimmer extends StatefulWidget {
+  static ShimmerState? of(BuildContext context) {
+    return context.findAncestorStateOfType<ShimmerState>();
+  }
+
+  const Shimmer({
+    super.key,
+    this.linearGradient = _shimmerGradient,
+    required this.child,
+  });
+
+  final LinearGradient linearGradient;
+  final Widget child;
+
+  @override
+  ShimmerState createState() => ShimmerState();
+}
+
+class ShimmerState extends State<Shimmer> with SingleTickerProviderStateMixin {
+  late AnimationController _shimmerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _shimmerController = AnimationController.unbounded(vsync: this)
+      ..repeat(
+        min: -0.5,
+        max: 1.5,
+        period: const Duration(
+          milliseconds: 1000,
+        ),
+      );
+  }
+
+  @override
+  void dispose() {
+    _shimmerController.dispose();
+    super.dispose();
+  }
+
+  LinearGradient get gradient => LinearGradient(
+        colors: widget.linearGradient.colors,
+        stops: widget.linearGradient.stops,
+        begin: widget.linearGradient.begin,
+        end: widget.linearGradient.end,
+        transform: _SlidingGradientTransform(
+          slidePercent: _shimmerController.value,
+        ),
+      );
+
+  bool get isSized =>
+      (context.findRenderObject() as RenderBox?)?.hasSize ?? false;
+
+  Size get size => (context.findRenderObject() as RenderBox).size;
+
+  Offset getDescendantOffset(
+      {required RenderBox descendant, Offset offset = Offset.zero}) {
+    final shimmerBox = context.findRenderObject() as RenderBox;
+    return descendant.localToGlobal(offset, ancestor: shimmerBox);
+  }
+
+  Listenable get shimmerChanges => _shimmerController;
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
+
+class ShimmerLoading extends StatefulWidget {
+  final bool isLoading;
+  final Widget loadingChild;
+  final Widget defaultChild;
+
+  const ShimmerLoading({
+    super.key,
+    required this.isLoading,
+    required this.loadingChild,
+    required this.defaultChild,
+  });
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Listenable? _shimmerChanges;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_shimmerChanges != null) {
+      _shimmerChanges!.removeListener(_onShimmerChange);
+    }
+    _shimmerChanges = Shimmer.of(context)?.shimmerChanges;
+    if (_shimmerChanges != null) {
+      _shimmerChanges!.addListener(_onShimmerChange);
+    }
+  }
+
+  void _onShimmerChange() {
+    if (widget.isLoading) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _shimmerChanges?.removeListener(_onShimmerChange);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final shimmer = Shimmer.of(context);
+    if (shimmer == null || !shimmer.isSized) {
+      // Return an empty box if Shimmer context is null
+      return const SizedBox();
+    }
+    if (!widget.isLoading) {
+      return widget.defaultChild;
+    }
+    final shimmerSize = shimmer.size;
+    final gradient = shimmer.gradient;
+    final renderObject = context.findRenderObject();
+    Offset offsetWithinShimmer = Offset.zero;
+    if (renderObject is RenderBox) {
+      offsetWithinShimmer = shimmer.getDescendantOffset(
+        descendant: renderObject,
+      );
+    }
+
+    return ShaderMask(
+      shaderCallback: (bounds) {
+        return gradient.createShader(
+          Rect.fromLTWH(
+            -offsetWithinShimmer.dx,
+            -offsetWithinShimmer.dy,
+            shimmerSize.width,
+            shimmerSize.height,
+          ),
+        );
+      },
+      blendMode: BlendMode.srcATop,
+      child: widget.loadingChild,
+    );
+  }
+}
--- test/shimmer_loading_animation_test.dart
+++ test/shimmer_loading_animation_test.dart
@@ -1,12 +1,78 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:shimmer_loading_animation/shimmer_loading_animation.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('ShimmerLoading shows loadingChild when isLoading is true',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Shimmer(
+          child: ShimmerLoading(
+            isLoading: true,
+            loadingChild: Text('Loading...'),
+            defaultChild: Text('Loaded'),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('Loading...'), findsOneWidget);
+    expect(find.text('Loaded'), findsNothing);
+  });
+
+  testWidgets('ShimmerLoading shows defaultChild when isLoading is false',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Shimmer(
+          child: ShimmerLoading(
+            isLoading: false,
+            loadingChild: Text('Loading...'),
+            defaultChild: Text('Loaded'),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('Loading...'), findsNothing);
+    expect(find.text('Loaded'), findsOneWidget);
+  });
+
+  testWidgets('ShaderMask is applied to loadingChild',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Shimmer(
+          child: ShimmerLoading(
+            isLoading: true,
+            loadingChild: Text('Loading...'),
+            defaultChild: Text('Loaded'),
+          ),
+        ),
+      ),
+    );
+
+    final shimmerLoadingFinder = find.byType(ShimmerLoading);
+    final shaderMaskFinder = find.descendant(
+        of: shimmerLoadingFinder, matching: find.byType(ShaderMask));
+
+    expect(shaderMaskFinder, findsOneWidget);
+  });
+
+  testWidgets('ShimmerLoading does not crash when Shimmer context is null',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ShimmerLoading(
+          isLoading: true,
+          loadingChild: Text('Loading...'),
+          defaultChild: Text('Loaded'),
+        ),
+      ),
+    );
+
+    expect(find.text('Loading...'), findsNothing);
+    expect(find.text('Loaded'), findsOneWidget);
   });
 }
